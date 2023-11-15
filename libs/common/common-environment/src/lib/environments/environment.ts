816f439a3e93b66d78a8a3ccb68684793f1b7361
Charlie Greenman <charlie@razroo.com>
2023-11-15 13:32:31 -0500

    Add code for auth-0-angular-16.2.0 add-auth-environment-configs.
    [Url: razroo.com/razroo/auth-0-angular-16.2.0/add-auth-config/add-auth-environment-configs]

--- libs/common/common-environment/src/lib/environments/environment.ts
+++ libs/common/common-environment/src/lib/environments/environment.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: false
+  production: false,
+  issuer: 'https://your-issuer-url.com',
+  clientId: 'yourClientIdABC1919ADADKDALA',
+  hostUrl: 'http://localhost:4200',
+  apiIdentifier: 'https://your-api-identifier.us.auth0.com/api/v2/',
+  graphQLURI: 'https://rickandmortyapi.com/graphql'
 }
