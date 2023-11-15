2582c6f6f407471ee620a3463136053237153ab9
Charlie Greenman <charlie@razroo.com>
2023-11-15 13:27:03 -0500

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/test-pronto-scaffold-logic/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-pronto-scaffold-logic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-pronto-scaffold-logic';
+}
