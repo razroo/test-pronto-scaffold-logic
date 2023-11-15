539223229eecaff8e6c538ea415c43860664b0e0
Charlie Greenman <charlie@razroo.com>
2023-11-15 13:32:56 -0500

    Add code for auth-0-angular-16.2.0 add-authentication-event-handlers.
    [Url: razroo.com/razroo/auth-0-angular-16.2.0/add-login-ui/add-authentication-event-handlers]

--- apps/test-pronto-scaffold-logic/src/app/app.component.ts
+++ apps/test-pronto-scaffold-logic/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from "@test-pronto-scaffold-logic/common/common-services";
 
 @Component({
   selector: 'test-pronto-scaffold-logic-root',
@@ -11,5 +12,16 @@ export class AppComponent {
 
   sideNavToggle(): void {
     this.sideNavOpen = !this.sideNavOpen;
+  }
+
+  constructor(private authService: AuthService) {
+  }
+
+  logIn() {
+    this.authService.loginWithRedirect()
+  }
+
+  signUp() {
+    this.authService.signUpWithRedirect();
   }
 }
